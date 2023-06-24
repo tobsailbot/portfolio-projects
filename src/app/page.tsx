6499a7e3f2a57c1eeabd73e7b965a9acf4ccb980
tobsailbot <tobsailbot@gmail.com>
2023-06-24 01:23:36 -0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,117 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+<div className='container text-light' style={{maxWidth:'880px'}}>
+
+  <br />
+
+  <div className='rounded-card container-sm mt-5 p-2 border  border-dark rounded-4'>
+
+    <div className='m-auto text-center' style={{position:'relative',top:'-50px', height:'115px'}}>
+      <img className='border border-5 border-secondary' width={150} src="https://www.w3schools.com/howto/img_avatar.png" alt="" style={{borderRadius:'100%', width:'150px', height:'150px'}}/>
+    </div>
+
+    <h2 className='text-center'>
+      TOBIAS PILARCHE
+    </h2>
+
+    <p className='text-center'> DEV JR. </p>
+
+    <div className='p-2 mx-4'>
+      <p className='about'>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.Unde recusandae sunt laboriosam magni, eveniet ipsam quas hic aliquam repellendus cum sit voluptatem voluptas ea dolorem porro! 
+      </p>
+    </div>
+
+
+    <br />
+    <br />
+    <div className="mx-4 border-top border-secondary"></div>
+    <br />
+    <br />
+
+    <div className="mx-4">
+
+      <h4>Projects</h4>
+      <br />
+
+        <div className='proj-card row border p-3 mx-1 border rounded-4 border-0'>
+
+          <div className="row text-center m-auto py-1"><p className='m-0 p-0'>PROJ TITLE</p></div>
+
+          <div className="row m-auto ">
+
+            <div className="col m-auto text-center py-2 ">
+              <img src="https://getuikit.com/v2/docs/images/placeholder_600x400_2.svg" alt="" height={200} width={300} style={{borderRadius:'20px'}}/>
+            </div>
+            <div className="col py-2">
+              <p className='about'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, sint deserunt expedita ipsam distinctio error explicabo illum minima deleniti autem, officiis in magni labore consequuntur nemo dolores saepe, dignissimos quia?
+              </p>
+            </div>
+
+          </div>
         </div>
-      </div>
+        <br />
+        <br />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className='proj-card row border p-3 mx-1 border rounded-4 border-0'>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <div className="row text-center m-auto py-1"><p className='m-0 p-0'>PROJ TITLE</p></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <div className="row m-auto ">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+            <div className="col m-auto text-center py-2 ">
+              <img src="https://getuikit.com/v2/docs/images/placeholder_600x400_2.svg" alt="" height={200} width={300} style={{borderRadius:'20px'}}/>
+            </div>
+            <div className="col py-2">
+              <p className='about'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, sint deserunt expedita ipsam distinctio error explicabo illum minima deleniti autem, officiis in magni labore consequuntur nemo dolores saepe, dignissimos quia?
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <div className='proj-card row border p-3 mx-1 border rounded-4 border-0'>
+
+          <div className="row text-center m-auto py-1"><p className='m-0 p-0'>PROJ TITLE</p></div>
+
+          <div className="row m-auto ">
+
+            <div className="col m-auto text-center py-2 ">
+              <img src="https://getuikit.com/v2/docs/images/placeholder_600x400_2.svg" alt="" height={200} width={300} style={{borderRadius:'20px'}}/>
+            </div>
+            <div className="col py-2">
+              <p className='about'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, sint deserunt expedita ipsam distinctio error explicabo illum minima deleniti autem, officiis in magni labore consequuntur nemo dolores saepe, dignissimos quia?
+              </p>
+            </div>
+
+          </div>
+        </div>
+
+        <br />
+
+
+        
+
+    </div>
+
+  </div>
+
+  <br />
+
+
+
+</div>
+
+
   )
 }
